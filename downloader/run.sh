b35928a35b17f357182d943a3c2d09ce52a2287b
Abdelsalam-Abbas <abdelsalam50@gmail.com>
2017-02-17 22:50:37 +0200

    first commit

--- downloader/run.sh
+++ downloader/run.sh
@@ -1,2 +1,5 @@
-wget https://wordpress.org/latest.tar.gz
-tar -xzvf latest.tar.gz --strip-component=1
+if [ ! -f /var/www/app/latest.tar.gz ] # don't download it if it is already exist 
+then
+	wget https://wordpress.org/latest.tar.gz
+	tar -xzvf latest.tar.gz --strip-component=1
+fi
--- phpfpm/run.sh
+++ phpfpm/run.sh
@@ -7,12 +7,26 @@ WORDPRESS_DB=$DB_ENV_WPDB
 WORDPRESS_USER=$DB_ENV_WPUSER
 WORDPRESS_PASS=$DB_ENV_WPPASS
 WORDPRESS_HOST=$DB_PORT_3306_TCP_ADDR
+if [ -z $WORDPRESS_HOST ] ## in case of using docker compose which will not import environment variables
+then
+	echo "getting ip of mysql contianer"
+	while  [[ ! $(ping -c mysql) ]] 
+	do 
+		echo "mysql server is not up yet. waiting..."
+	        sleep 1
+	done
+	WORDPRESS_HOST=$(getent hosts mysql | awk '{ print $1}') 
+	echo "ip add is $WORDPRESS_HOST"
+fi
 
 echo 'Check for the wp-config.php'
-if [ -f $WORDPRESS_DIR/wp-config.php ]
-then
-echo 'No need to edit the config file... starting php-fpm'
-else
+#else
+while [ ! -f $WORDPRESS_DIR/wp-config-sample.php ] # waiting for downloader container to download files 
+do
+	echo "waiting for wordpress files to be downloaded"
+	sleep 2   
+done
+
 echo 'Editing wp-config.php...'
 echo 'Starting the service'
 cp $WORDPRESS_DIR/wp-config-sample.php $WORDPRESS_DIR/wp-config.php
@@ -20,10 +34,9 @@ sed -i "s/database_name_here/$WORDPRESS_DB/" /var/www/app/wp-config.php
 sed -i "s/username_here/$WORDPRESS_USER/" /var/www/app/wp-config.php
 sed -i "s/password_here/$WORDPRESS_PASS/" /var/www/app/wp-config.php
 sed -i "s/localhost/$WORDPRESS_HOST/" /var/www/app/wp-config.php
-fi
+#fi
 sleep 5
 
 #starting php-fpm in foreground
 
 /usr/sbin/php5-fpm -F
-
